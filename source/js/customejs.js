288e665522e6b9a4f51ec38ea57068076f188727
unknown <gmzulkar@gmail.com>
2022-06-22 17:13:13 +0600

    update

--- source/js/customejs.js
+++ source/js/customejs.js
@@ -7,38 +7,7 @@ navLink.addEventListener('click', function (e) {
     });
     dataId.classList.add('active');
 
-    switch (parseInt(dataId.id.split('-')[1])) {
+    serviceDetails[parseInt(dataId.id.split('-')[1])].classList.add('show');
+});
 
-        case 1:
-            // code block
-            serviceDetails[0].classList.add('show');
-            break;
-        case 2:
-            // code block
-            serviceDetails[1].classList.add('show');
-            break;
-        case 3:
-            // code block
-            serviceDetails[2].classList.add('show');
-            break;
-        case 4:
-            // code block
-            serviceDetails[3].classList.add('show');
-            break;
-        case 5:
-            // code block
-            serviceDetails[4].classList.add('show');
-            break;
-        case 6:
-            // code block
-            serviceDetails[5].classList.add('show');
-            break;
-        case 7:
-            // code block
-            serviceDetails[6].classList.add('show');
-            break;
-        default:
-        // code block
-    }
 
-})
