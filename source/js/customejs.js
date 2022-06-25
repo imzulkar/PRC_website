b5a7b9dad7dc52a1babd68d041c409342e2a36fd
unknown <gmzulkar@gmail.com>
2022-06-25 11:38:55 +0600

    nav bar and services fixed

--- source/js/customejs.js
+++ source/js/customejs.js
@@ -5,9 +5,13 @@ navLink.addEventListener('click', function (e) {
     serviceDetails.forEach(function (item) {
         item.classList.remove('show');
     });
+    const allNavItem = document.querySelectorAll('.nav-link');
+    allNavItem.forEach(function (listItem) {
+        listItem.classList.remove('active');
+    })
     dataId.classList.add('active');
 
-    serviceDetails[parseInt(dataId.id.split('-')[1])].classList.add('show');
+    serviceDetails[parseInt(dataId.id.split('-')[1]) - 1].classList.add('show');
 });
 
 
